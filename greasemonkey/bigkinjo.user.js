807063e44f14220054c4c47a3fa3e5f28fa29316
硫酸鶏 <root@acid-chicken.com>
2017-04-23 22:44:02 +0900

    Update bigkinjo.user.js

--- greasemonkey/bigkinjo.user.js
+++ greasemonkey/bigkinjo.user.js
@@ -4,7 +4,7 @@
 // @description アメちゃんを左下にピタッ！ご近所さんを真ん中にドーン！
 // @include     https://mstdn.osaka/*
 // @require     https://code.jquery.com/jquery-1.11.1.min.js
-// @version     1.2
+// @version     1.3
 // @grant       none
 // ==/UserScript==
 $(document).on('ready page:load',() => {
@@ -14,8 +14,9 @@ $(document).on('ready page:load',() => {
     $('.column-icon-clear').css('top', '0'); // 消しゴムのズレを直すで
     $('.drawer').css('display', 'flex'); // 書き込み欄をflex配置にすんで
     $('.drawer__pager').css('flex', '0 0 237px'); // 書き込み欄が消えんのを防ぐで
-    $('.ui').css({ // Wikimediaのパブリックライセンスの大阪城の写真を借りてくんで
-      'background-image': 'url("//upload.wikimedia.org/wikipedia/commons/f/f6/Osaka_Castle_Sakura_April_2005.JPG")',
+    $('.ui').css({
+      'background-image': 'url(//mstdn.osaka/assets/background-photo-4d5afc27a5805bfb866b8f7f5067c66af99595cdc8dc46ece07fb5ff375cc3d5.png)',
+      'background-position': 'right bottom',
       'background-repeat': 'no-repeat',
       'background-size': 'cover'
     });
@@ -31,7 +32,7 @@ $(document).on('ready page:load',() => {
       if (window.matchMedia('(min-width: 1025px)').matches) { // ページを移動すると元に戻ってしまうのを防ぐで
         $('.columns-area').children().css({
           'flex': '1', // 全体をflexで伸びるようにすんで
-          'opacity': '.96' // 4%ほど透かすで
+          'opacity': '.8' // 20%ほど透かすで
         });
         $('.column:eq(2)').css('flex', '2'); // 近所のサイズを2倍にすんで
       }
