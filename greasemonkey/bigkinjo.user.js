c3f40c67b9c886f39283eb245f9be50e93c3c909
硫酸鶏 <root@acid-chicken.com>
2017-04-24 21:07:56 +0900

    Update bigkinjo.user.js

--- greasemonkey/bigkinjo.user.js
+++ greasemonkey/bigkinjo.user.js
@@ -4,21 +4,20 @@
 // @description アメちゃんを左下にピタッ！ご近所さんを真ん中にドーン！
 // @include     https://mstdn.osaka/*
 // @require     https://code.jquery.com/jquery-1.11.1.min.js
-// @version     1.3
+// @version     1.4
 // @grant       none
 // ==/UserScript==
+const home = '<a class="drawer__tab" href="javascript:Home()" title="ウチの表示切り替え"><i class="fa fa-fw fa-home"></i></a>';
+const script = '<script>\n  function Home() {\n    if ($(\'#home\').css(\'display\') === \'none\') $(\'#home\').show()\n    else $(\'#home\').hide()\n  }\n</script>';
 $(document).on('ready page:load',() => {
   if (window.matchMedia('(min-width: 1025px)').matches) {
-    $('.column:eq(0)').hide(); // ウチを消すで
+    $('body').append(script); // ウチのスクリプトを追加すんで
+    $('.column:eq(0)').attr('id', 'home'); // ウチ欄をウチと命名すんで
     $('.column:eq(1)').attr('id', 'candy'); // 通知欄をアメちゃんと命名すんで
     $('.column-icon-clear').css('top', '0'); // 消しゴムのズレを直すで
     $('.drawer').css('display', 'flex'); // 書き込み欄をflex配置にすんで
     $('.drawer__pager').css('flex', '0 0 237px'); // 書き込み欄が消えんのを防ぐで
-    $('.ui').css({
-      'background-image': 'url(//mstdn.osaka/assets/background-photo-4d5afc27a5805bfb866b8f7f5067c66af99595cdc8dc46ece07fb5ff375cc3d5.png)',
-      'background-repeat': 'no-repeat',
-      'background-size': 'cover'
-    });
+    $('.drawer__tab:eq(2)').after(home); // 書き込み欄のタブ上の四番目にウチボタンを追加すんで
     $('#candy').appendTo('.drawer'); // アメちゃんを書き込み欄に突っ込むで
     $('#candy').css({ // 突っ込むにあたっていろいろと設定すんで
       'flex': '1 1 auto',
@@ -27,12 +26,10 @@ $(document).on('ready page:load',() => {
       'margin-top': '10px',
       'width': 'initial'
     });
+    $('#home').hide(); // ウチを消すで
     setInterval(() => {
       if (window.matchMedia('(min-width: 1025px)').matches) { // ページを移動すると元に戻ってしまうのを防ぐで
-        $('.columns-area').children().css({
-          'flex': '1', // 全体をflexで伸びるようにすんで
-          'opacity': '.8' // 20%ほど透かすで
-        });
+        $('.columns-area').children().css('flex', '1'); // 全体をflexで伸びるようにすんで
         $('.column:eq(2)').css('flex', '2'); // 近所のサイズを2倍にすんで
       }
     }, 16);
